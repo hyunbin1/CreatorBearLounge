2a16a445ccb286889bfa8f85ca8f38c1b09b876c
hyunbin1 <kimgusqls1@gmail.com>
2022-04-17 16:54:59 +0900

    [add] figure out How to take datas about bought NFT in Connected user

---
+++ creator-bear-rounge/src/NFTCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NFTCard = ({ nft }) => {
+  return <div>{nft.meta.name}</div>;
+};
+
+export default NFTCard;
--- creator-bear-rounge/src/WalletCard.js
+++ creator-bear-rounge/src/WalletCard.js
@@ -1,32 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-// import "./WalletCard.css";
+import NFTContainer from "./NFTContainer";
 
 const WalletCard = () => {
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [walletAddress, setWalletAddress] = useState(null);
   const [userBalance, setUserBalance] = useState(null);
+  const [nfts, setNfts] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
   const [connectButtonText, setConnectButtonText] = useState("Connect Wallet");
 
   /* Method:
-      1. ConnectWalletHandler
-      2. account ChangedHandler
-      3. getAccountBalance
-      4. chainChangedHandler 
+      1. connectWallet
+      2. getNftData
+      3. account ChangedHandler
+      4. getAccountBalance
+      5. chainChangedHandler 
   */
 
   // Method 1.
-  const connectWalletHandler = () => {
+  const connectWallet = async () => {
     if (window.ethereum && window.ethereum.isMetaMask) {
       console.log("Make MetaMask New Account is Here!");
 
-      window.ethereum
-        .request({ method: "eth_requestAccounts" }) // 새 계정 만들도록 요청
-        .then((newAccount) => {
-          accountChangedHandler(newAccount[0]); // 새로운 계정으로 변경하여 가져오기
-          setConnectButtonText("Wallet Connected");
-          getAccountBalance(newAccount[0]); // 새로 만든 계정의 Balance 가져오기
-        });
+      // 계정 연결 OR 새 계정 만들도록 요청
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setWalletAddress(accounts[0]);
     } else {
       // 메타마스크 설치가 안되어 있을 경우 에러메세지
       console.log("Need to install MetaMask");
@@ -34,13 +34,33 @@ const WalletCard = () => {
     }
   };
 
-  // Method 2. 계정 업데이트 메서드, re-render 할 때 발생함
+  // Method 2. user가 가지고있는 nft 정보 가져오기
+  const getNftData = async () => {
+    if (!walletAddress) return;
+
+    // 참조 API: https://api.rarible.org/v0.1/doc#operation/getItemByIds
+    const response = await fetch(
+      `https://api.rarible.org/v0.1/items/byOwner/?owner=ETHEREUM:0x60f80121c31a0d46b5279700f9df786054aa5ee5`
+      // `https://api.rarible.org/v0.1/items/byOwner/?owner=ETHEREUM:${walletAddress}`
+    );
+
+    const data = await response.json();
+
+    setNfts(data.items);
+    debugger;
+  };
+
+  useEffect(() => {
+    getNftData();
+  }, [walletAddress]);
+
+  // Method 3. 계정 업데이트 메서드, re-render 할 때 발생함
   const accountChangedHandler = (newAccount) => {
-    setDefaultAccount(newAccount);
+    setWalletAddress(newAccount);
     getAccountBalance(newAccount.toString());
   };
 
-  // Method 3. 계정 Balance 가져오기
+  // Method 4. 계정 Balance 가져오기
   const getAccountBalance = (account) => {
     window.ethereum
       .request({ method: "eth_getBalance", params: [account, "latest"] })
@@ -52,7 +72,7 @@ const WalletCard = () => {
       });
   };
 
-  // Method 4. reload the page to avoid any errors with chain change mid use of application
+  // Method 5. reload the page to avoid any errors with chain change mid use of application
   const chainChangedHandler = () => {
     window.location.reload();
   };
@@ -63,11 +83,13 @@ const WalletCard = () => {
   // 화면에 보이는 모습
   return (
     <div className="walletCard">
-      <h4> {"window.ethereum methods를 이용하여 메타마스트와 연결"} </h4>
-      <button onClick={connectWalletHandler}>{connectButtonText}</button>
+      <h4> {"window.ethereum 메타마스트와 연결"} </h4>
+      <button onClick={connectWallet}>{connectButtonText}</button>
       <div className="accountDisplay">
-        <h3> Address: {defaultAccount}</h3>
+        <h3> Address: {walletAddress}</h3>
       </div>
+
+      <NFTContainer nfts={nfts} />
       <div className="balanceDisplay">
         <h3> balance: {userBalance}</h3>
       </div>
